22ea881c1c0800d97f38e5fbfcb6db2dac02ffea
ReggieMartin <59172257+ReggieMartin@users.noreply.github.com>
2022-03-03 23:03:47 -0500

    Add files via upload

---
+++ cf_recommender.py
@@ -0,0 +1,20 @@
+import fastbook
+from fastbook import *
+from fastai.collab import *
+
+ratings = pd.read_csv('take_home_ss_ratings.xls')  
+dls = CollabDataLoaders.from_df(ratings, valid_pct=0.2, user_name='user_id', item_name='item_id', rating_name='rating', bs=64)
+
+#Create matrix factorization learner with 20 factors, 
+learn = collab_learner(dls, n_factors=20, y_range=(0, 1.0))
+learn.fit_one_cycle(5, 2e-2, wd=0.1)
+
+#Need to create a copy to get predictions because of a small FastAI bug:
+new_df = ratings.copy()
+dl = learn.dls.test_dl(new_df)
+
+#Print a sample of the input, output, and ground truth:
+inp, preds, targets = learn.get_preds(with_input=True, dl=dl)
+print("       First 15 inputs\n      (user_id, item_id):\n", inp[0:15])
+print("First 15 predictions:\n", preds[0:15])
+print("First 15 actual ratings (targets):\n", targets[0:15])
